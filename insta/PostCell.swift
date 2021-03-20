aa78a9b9e435102be52d550542bab263de7b4d9f
Mark <iammarkcruz@gmail.com>
2021-03-19 19:27:57 -0700

    fixed broken images

---
+++ insta/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  insta
+//
+//  Created by Mark Cruz on 3/16/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
